Implemented character CNN (first pass). Fixed start_end_pos(). Fixed self attn masking issues.

code/data_batcher.py
@@ -26,16 +26,26 @@ import numpy as np
 from six.moves import xrange
 from vocab import PAD_ID, UNK_ID
 
+char_vocab = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+         "p", "q", "r", "s", "t", "u", "v", "x", "y", "z", "0", "1", "2", "3", "4",
+         "5", "6", "7", "8", "9", ",", ".", "!", "(", ")", "[", "]", "%", "'", "-",
+         "/", '"', "?", "$"]
+char2id = {char: i+2 for i, char in enumerate(char_vocab)} # + 2 for pad and unk IDs (0 and 1 respectively -- same as words)
 
 class Batch(object):
     """A class to hold the information needed for a training batch"""
 
-    def __init__(self, context_ids, context_mask, context_tokens, qn_ids, qn_mask, qn_tokens, ans_span, ans_tokens, uuids=None):
+    def __init__(self, context_ids, context_mask, context_tokens, context_char_ids, context_char_mask, \
+                 qn_ids, qn_mask, qn_tokens, qn_char_ids, qn_char_mask, ans_span, ans_tokens, uuids=None):
         """
         Inputs:
           {context/qn}_ids: Numpy arrays.
             Shape (batch_size, {context_len/question_len}). Contains padding.
           {context/qn}_mask: Numpy arrays, same shape as _ids.
+            Contains 1s where there is real data, 0s where there is padding.
+          {context/qn}_char_ids: Numpy arrays.
+            Shape (batch_size, {context_len/question_len}, word_len). Contains padding.
+          {context/qn}_char_mask: Numpy arrays, same shape as _char_ids.
             Contains 1s where there is real data, 0s where there is padding.
           {context/qn/ans}_tokens: Lists length batch_size, containing lists (unpadded) of tokens (strings)
           ans_span: numpy array, shape (batch_size, 2)
@@ -44,10 +54,14 @@ class Batch(object):
         """
         self.context_ids = context_ids
         self.context_mask = context_mask
+        self.context_char_ids = context_char_ids
+        self.context_char_mask = context_char_mask
         self.context_tokens = context_tokens
 
         self.qn_ids = qn_ids
         self.qn_mask = qn_mask
+        self.qn_char_ids = qn_char_ids
+        self.qn_char_mask = qn_char_mask
         self.qn_tokens = qn_tokens
 
         self.ans_span = ans_span
@@ -79,6 +93,16 @@ def sentence_to_token_ids(sentence, word2id):
     ids = [word2id.get(w, UNK_ID) for w in tokens]
     return tokens, ids
 
+def sentence_to_char_ids(sentence):
+    """Turns an already-tokenized sentence string into char indices
+    e.g. "i do n't know" -> [[0], [1,2], [3,4], [5,3,2,6]]
+    Note that any character that isn't in the char2id mapping gets map to the id for UNK
+    """
+    tokens = split_by_whitespace(sentence) # list of strings
+    char_ids = []
+    for token in tokens:
+        char_ids.append([char2id.get(c, UNK_ID) for c in token])
+    return char_ids
 
 def padded(token_batch, batch_pad=0):
     """
@@ -93,7 +117,7 @@ def padded(token_batch, batch_pad=0):
     return map(lambda token_list: token_list + [PAD_ID] * (maxlen - len(token_list)), token_batch)
 
 
-def refill_batches(batches, word2id, context_file, qn_file, ans_file, batch_size, context_len, question_len, discard_long):
+def refill_batches(batches, word2id, context_file, qn_file, ans_file, batch_size, context_len, question_len, word_len, discard_long):
     """
     Adds more batches into the "batches" list.
 
@@ -115,7 +139,9 @@ def refill_batches(batches, word2id, context_file, qn_file, ans_file, batch_size
 
         # Convert tokens to word ids
         context_tokens, context_ids = sentence_to_token_ids(context_line, word2id)
+        context_char_ids = sentence_to_char_ids(context_line)
         qn_tokens, qn_ids = sentence_to_token_ids(qn_line, word2id)
+        qn_char_ids = sentence_to_char_ids(context_line)
         ans_span = intstr_to_intlist(ans_line)
 
         # read the next line from each file
@@ -142,8 +168,17 @@ def refill_batches(batches, word2id, context_file, qn_file, ans_file, batch_size
             else: # truncate
                 context_ids = context_ids[:context_len]
 
+        qn_char_ids = qn_char_ids[:question_len]
+        for i in xrange(len(qn_char_ids)):
+            if len(qn_char_ids[i]) > word_len:
+                qn_char_ids[i] = qn_char_ids[i][:word_len]
+        context_char_ids = context_char_ids[:context_len]
+        for i in xrange(len(context_char_ids)):
+            if len(context_char_ids[i]) > word_len:
+                context_char_ids[i] = context_char_ids[i][:word_len]
+
         # add to examples
-        examples.append((context_ids, context_tokens, qn_ids, qn_tokens, ans_span, ans_tokens))
+        examples.append((context_ids, context_tokens, context_char_ids, qn_ids, qn_tokens, qn_char_ids, ans_span, ans_tokens))
 
         # stop refilling if you have 160 batches
         if len(examples) == batch_size * 160:
@@ -153,15 +188,18 @@ def refill_batches(batches, word2id, context_file, qn_file, ans_file, batch_size
 
     # Sort by question length
     # Note: if you sort by context length, then you'll have batches which contain the same context many times (because each context appears several times, with different questions)
-    examples = sorted(examples, key=lambda e: len(e[2]))
+    examples = sorted(examples, key=lambda e: len(e[3]))
 
     # Make into batches and append to the list batches
     for batch_start in xrange(0, len(examples), batch_size):
 
         # Note: each of these is a list length batch_size of lists of ints (except on last iter when it might be less than batch_size)
-        context_ids_batch, context_tokens_batch, qn_ids_batch, qn_tokens_batch, ans_span_batch, ans_tokens_batch = zip(*examples[batch_start:batch_start+batch_size])
+        context_ids_batch, context_tokens_batch, context_char_ids_batch, \
+            qn_ids_batch, qn_tokens_batch, qn_char_ids_batch, ans_span_batch, \
+            ans_tokens_batch = zip(*examples[batch_start:batch_start+batch_size])
 
-        batches.append((context_ids_batch, context_tokens_batch, qn_ids_batch, qn_tokens_batch, ans_span_batch, ans_tokens_batch))
+        batches.append((context_ids_batch, context_tokens_batch, context_char_ids_batch, \
+                        qn_ids_batch, qn_tokens_batch, qn_char_ids_batch, ans_span_batch, ans_tokens_batch))
 
     # shuffle the batches
     random.shuffle(batches)
@@ -171,7 +209,7 @@ def refill_batches(batches, word2id, context_file, qn_file, ans_file, batch_size
     return
 
 
-def get_batch_generator(word2id, context_path, qn_path, ans_path, batch_size, context_len, question_len, discard_long):
+def get_batch_generator(word2id, context_path, qn_path, ans_path, batch_size, context_len, question_len, word_len, discard_long):
     """
     This function returns a generator object that yields batches.
     The last batch in the dataset will be a partial batch.
@@ -190,30 +228,48 @@ def get_batch_generator(word2id, context_path, qn_path, ans_path, batch_size, co
 
     while True:
         if len(batches) == 0: # add more batches
-            refill_batches(batches, word2id, context_file, qn_file, ans_file, batch_size, context_len, question_len, discard_long)
+            refill_batches(batches, word2id, context_file, qn_file, ans_file, batch_size, context_len, question_len, word_len, discard_long)
         if len(batches) == 0:
             break
 
         # Get next batch. These are all lists length batch_size
-        (context_ids, context_tokens, qn_ids, qn_tokens, ans_span, ans_tokens) = batches.pop(0)
+        (context_ids, context_tokens, context_char_ids, qn_ids, qn_tokens, qn_char_ids, ans_span, ans_tokens) = batches.pop(0)
 
         # Pad context_ids and qn_ids
         qn_ids = padded(qn_ids, question_len) # pad questions to length question_len
         context_ids = padded(context_ids, context_len) # pad contexts to length context_len
 
+        actual_batch_size = len(qn_ids)
+        qn_char_ids_tmp = np.zeros((actual_batch_size, question_len, word_len))
+        context_char_ids_tmp = np.zeros((actual_batch_size, context_len, word_len))
+        for i in xrange(len(qn_char_ids)): # pad question character ids to length word_len
+            padded_qn = padded(qn_char_ids[i], word_len) # list of lists of length word_len
+            qn_char_ids_tmp[i,:len(padded_qn),:] = padded_qn
+        for i in xrange(len(context_char_ids)): # pad context character ids to length word_len
+            padded_ids = padded(context_char_ids[i], word_len) # list of lists of length word_len
+            context_char_ids_tmp[i,:len(padded_ids),:] = padded_ids
+
+        qn_char_ids = qn_char_ids_tmp
+        context_char_ids = context_char_ids_tmp
+
         # Make qn_ids into a np array and create qn_mask
         qn_ids = np.array(qn_ids) # shape (batch_size, question_len)
         qn_mask = (qn_ids != PAD_ID).astype(np.int32) # shape (batch_size, question_len)
+        qn_char_ids = np.array(qn_char_ids) # shape (batch_size, question_len, word_len)
+        qn_char_mask = (qn_char_ids != PAD_ID).astype(np.int32) # shape (batch_size, question_len, word_len)
 
         # Make context_ids into a np array and create context_mask
         context_ids = np.array(context_ids) # shape (batch_size, context_len)
         context_mask = (context_ids != PAD_ID).astype(np.int32) # shape (batch_size, context_len)
+        context_char_ids = np.array(context_char_ids)
+        context_char_mask = (context_char_ids != PAD_ID).astype(np.int32)
 
         # Make ans_span into a np array
         ans_span = np.array(ans_span) # shape (batch_size, 2)
 
         # Make into a Batch object
-        batch = Batch(context_ids, context_mask, context_tokens, qn_ids, qn_mask, qn_tokens, ans_span, ans_tokens)
+        batch = Batch(context_ids, context_mask, context_tokens, context_char_ids, context_char_mask, \
+                      qn_ids, qn_mask, qn_tokens, qn_char_ids, qn_char_mask, ans_span, ans_tokens)
 
         yield batch
 

code/modules.py
@@ -330,7 +330,7 @@ class SelfAttn(object):
         self.v_self = tf.get_variable("v_self", shape=(hidden_sz, 1), \
             initializer=tf.contrib.layers.xavier_initializer())
 
-    def build_graph(self, reprs):
+    def build_graph(self, reprs, reprs_mask):
         with vs.variable_scope("SelfAttn"):
             _, num_reprs, repr_sz = reprs.shape
 
@@ -343,16 +343,13 @@ class SelfAttn(object):
             flat_tmp3 = tf.reshape(tmp3, (-1, self.hidden_sz))
             e = tf.reshape(tf.matmul(flat_tmp3, self.v_self), (-1, num_reprs, num_reprs, )) # (batch_sz, num_reprs, num_reprs, )
 
-            mask = tf.ones_like(e)
-            _, attn_dist = masked_softmax(e, mask, 1)
-
+            reprs_attn_mask = tf.expand_dims(reprs_mask, 1)
+            _, attn_dist = masked_softmax(e, reprs_attn_mask, 1)
             self_outputs = tf.matmul(attn_dist, reprs)
 
             # concatenate self_outputs with reprs
             concat_outputs = tf.concat([reprs, self_outputs], 2) # (batch_sz, num_reprs, 2*repr_size)
-            rnn_mask = tf.ones_like(concat_outputs, dtype=tf.int32)
-            rnn_mask = tf.reduce_prod(rnn_mask, axis=2) # (batch_size, num_reprs, )
-            output = self.encoder.build_graph(concat_outputs, rnn_mask)
+            output = self.encoder.build_graph(concat_outputs, reprs_mask)
 
             return output
 

code/qa_model.py
@@ -90,8 +90,12 @@ class QAModel(object):
         # allows you to run the same model with variable batch_size
         self.context_ids = tf.placeholder(tf.int32, shape=[None, self.FLAGS.context_len])
         self.context_mask = tf.placeholder(tf.int32, shape=[None, self.FLAGS.context_len])
+        self.context_c_ids = tf.placeholder(tf.int32, shape=[None, self.FLAGS.context_len, self.FLAGS.word_len])
+        self.context_c_mask = tf.placeholder(tf.int32, shape=[None, self.FLAGS.context_len, self.FLAGS.word_len])
         self.qn_ids = tf.placeholder(tf.int32, shape=[None, self.FLAGS.question_len])
         self.qn_mask = tf.placeholder(tf.int32, shape=[None, self.FLAGS.question_len])
+        self.qn_c_ids = tf.placeholder(tf.int32, shape=[None, self.FLAGS.question_len, self.FLAGS.word_len])
+        self.qn_c_mask = tf.placeholder(tf.int32, shape=[None, self.FLAGS.question_len, self.FLAGS.word_len])
         self.ans_span = tf.placeholder(tf.int32, shape=[None, 2])
 
         # Add a placeholder to feed in the keep probability (for dropout).
@@ -107,8 +111,7 @@ class QAModel(object):
           emb_matrix: shape (400002, embedding_size).
             The GloVe vectors, plus vectors for PAD and UNK.
         """
-        with vs.variable_scope("embeddings"):
-
+        with vs.variable_scope("embeddings", reuse=tf.AUTO_REUSE):
             # Note: the embedding matrix is a tf.constant which means it's not a trainable parameter
             if self.FLAGS.retrain_word_embeds:
                 embedding_matrix = tf.get_variable("emb_matrix", shape=(400002, self.FLAGS.embedding_size), \
@@ -120,6 +123,30 @@ class QAModel(object):
             # using the placeholders self.context_ids and self.qn_ids
             self.context_embs = embedding_ops.embedding_lookup(embedding_matrix, self.context_ids) # shape (batch_size, context_len, embedding_size)
             self.qn_embs = embedding_ops.embedding_lookup(embedding_matrix, self.qn_ids) # shape (batch_size, question_len, embedding_size)
+
+            if self.FLAGS.char_embeddings:
+                # Note 51 = char vocab size (49 chars, plus PAD + UNK)
+                char_emb_matrix = tf.get_variable("char_emb_matrix", shape=(51, self.FLAGS.char_embedding_size), \
+                        initializer=tf.contrib.layers.xavier_initializer())
+
+                self.context_c_embs = embedding_ops.embedding_lookup(char_emb_matrix, self.context_c_ids) # shape (batch_size, context_len, word_len, char_embedding_size)
+                self.qn_c_embs = embedding_ops.embedding_lookup(char_emb_matrix, self.qn_c_ids) # shape (batch_size, question_len, word_len, char_embedding_size)
+
+                # First reshape c_embs to (batch_size * {context/question}_len, word_len, char_embedding_size
+                max_word_len = self.context_c_embs.shape[2] # == self.qn_c_embs.shape[2]
+                self.context_c_embs = tf.reshape(self.context_c_embs, (-1, max_word_len, self.FLAGS.char_embedding_size))
+                self.context_c_embs = tf.layers.conv1d(self.context_c_embs, self.FLAGS.char_num_filters, self.FLAGS.char_kernel_size, padding="same")
+                self.context_c_embs = tf.reshape(self.context_c_embs, (-1, self.FLAGS.context_len, max_word_len, self.FLAGS.char_num_filters)) # shape (batch_size, context_len, word_len, char_num_filters)
+                self.context_c_embs = tf.reduce_max(self.context_c_embs, 2) # shape (batch_size, context_len, char_num_filters)
+
+                # print self.context_c_embs
+                self.qn_c_embs = tf.reshape(self.qn_c_embs, (-1, max_word_len, self.FLAGS.char_embedding_size))
+                self.qn_c_embs = tf.layers.conv1d(self.qn_c_embs, self.FLAGS.char_num_filters, self.FLAGS.char_kernel_size, padding="same")
+                self.qn_c_embs = tf.reshape(self.qn_c_embs, (-1, self.FLAGS.question_len, max_word_len, self.FLAGS.char_num_filters)) # shape (batch_size, question_len, word_len, char_num_filters)
+                self.qn_c_embs = tf.reduce_max(self.qn_c_embs, axis=2) # shape (batch_size, question_len, char_num_filters)
+
+                self.context_embs = tf.concat([self.context_embs, self.context_c_embs], 2)
+                self.qn_embs = tf.concat([self.qn_embs, self.qn_c_embs], 2)
 
 
     def build_graph(self):
@@ -139,8 +166,6 @@ class QAModel(object):
         encoder = StackedRNNEncoder(self.FLAGS.hidden_size, self.FLAGS.num_encoding_layers, self.keep_prob)
         context_hiddens = encoder.build_graph(self.context_embs, self.context_mask) # (batch_size, context_len, hidden_size*2)
         question_hiddens = encoder.build_graph(self.qn_embs, self.qn_mask) # (batch_size, question_len, hidden_size*2)
-        print context_hiddens
-        print question_hiddens
 
         # Use context hidden states to attend to question hidden states
         if self.FLAGS.bidaf:
@@ -155,7 +180,7 @@ class QAModel(object):
         # Self-attention layer
         if self.FLAGS.self_attend:
             self_attn_layer = SelfAttn(self.keep_prob, blended_reps.shape[-1], self.FLAGS.self_attend_hidden_sz)
-            blended_reps = self_attn_layer.build_graph(blended_reps) # (batch_size, context_len, 2*self_attend_hidden_sz)
+            blended_reps = self_attn_layer.build_graph(blended_reps, self.context_mask) # (batch_size, context_len, 2*self_attend_hidden_sz)
 
         # Apply fully connected layer to each blended representation
         # Note, blended_reps_final corresponds to b' in the handout
@@ -236,8 +261,10 @@ class QAModel(object):
         input_feed = {}
         input_feed[self.context_ids] = batch.context_ids
         input_feed[self.context_mask] = batch.context_mask
+        input_feed[self.context_c_ids] = batch.context_char_ids
         input_feed[self.qn_ids] = batch.qn_ids
         input_feed[self.qn_mask] = batch.qn_mask
+        input_feed[self.qn_c_ids] = batch.qn_char_ids
         input_feed[self.ans_span] = batch.ans_span
         input_feed[self.keep_prob] = 1.0 - self.FLAGS.dropout # apply dropout
 
@@ -268,8 +295,10 @@ class QAModel(object):
         input_feed = {}
         input_feed[self.context_ids] = batch.context_ids
         input_feed[self.context_mask] = batch.context_mask
+        input_feed[self.context_c_ids] = batch.context_char_ids
         input_feed[self.qn_ids] = batch.qn_ids
         input_feed[self.qn_mask] = batch.qn_mask
+        input_feed[self.qn_c_ids] = batch.qn_char_ids
         input_feed[self.ans_span] = batch.ans_span
         # note you don't supply keep_prob here, so it will default to 1 i.e. no dropout
 
@@ -294,8 +323,10 @@ class QAModel(object):
         input_feed = {}
         input_feed[self.context_ids] = batch.context_ids
         input_feed[self.context_mask] = batch.context_mask
+        input_feed[self.context_c_ids] = batch.context_char_ids
         input_feed[self.qn_ids] = batch.qn_ids
         input_feed[self.qn_mask] = batch.qn_mask
+        input_feed[self.qn_c_ids] = batch.qn_char_ids
         # note you don't supply keep_prob here, so it will default to 1 i.e. no dropout
 
         output_feed = [self.probdist_start, self.probdist_end]
@@ -323,14 +354,12 @@ class QAModel(object):
         # instead of taking argmax over entire end_dist:
         #    end_pos' = argmax(end_dist[start_pos:start_pos+15])
         # then the real end_pos = end_pos' + start_pos
-        print end_dist.shape
-        trunc_end_dist = np.array([np.array(end_dist[i,start:start+20]) for i,start in enumerate(start_pos)])
-        print trunc_end_dist.shape
-        end_pos_new = start_pos + np.argmax(trunc_end_dist, axis=1)
-        print end_pos_new.shape
-        end_pos = np.argmax(end_dist, axis=1)
-        print end_pos.shape
-
+        trunc_end_dist = np.zeros((start_dist.shape[0], 20))
+        for i,start in enumerate(start_pos):
+            row = end_dist[i, start:start+20]
+            trunc_end_dist[i, :len(row)] = row
+        end_pos = start_pos + np.argmax(trunc_end_dist, axis=1)
+        # end_pos = np.argmax(end_dist, axis=1)
         return start_pos, end_pos
 
 
@@ -354,7 +383,7 @@ class QAModel(object):
         # which are longer than our context_len or question_len.
         # We need to do this because if, for example, the true answer is cut
         # off the context, then the loss function is undefined.
-        for batch in get_batch_generator(self.word2id, dev_context_path, dev_qn_path, dev_ans_path, self.FLAGS.batch_size, context_len=self.FLAGS.context_len, question_len=self.FLAGS.question_len, discard_long=True):
+        for batch in get_batch_generator(self.word2id, dev_context_path, dev_qn_path, dev_ans_path, self.FLAGS.batch_size, context_len=self.FLAGS.context_len, question_len=self.FLAGS.question_len, word_len=self.FLAGS.word_len, discard_long=True):
 
             # Get loss for this batch
             loss = self.get_loss(session, batch)
@@ -409,7 +438,7 @@ class QAModel(object):
 
         # Note here we select discard_long=False because we want to sample from the entire dataset
         # That means we're truncating, rather than discarding, examples with too-long context or questions
-        for batch in get_batch_generator(self.word2id, context_path, qn_path, ans_path, self.FLAGS.batch_size, context_len=self.FLAGS.context_len, question_len=self.FLAGS.question_len, discard_long=False):
+        for batch in get_batch_generator(self.word2id, context_path, qn_path, ans_path, self.FLAGS.batch_size, context_len=self.FLAGS.context_len, question_len=self.FLAGS.question_len, word_len=self.FLAGS.word_len, discard_long=False):
 
             pred_start_pos, pred_end_pos = self.get_start_end_pos(session, batch)
 
@@ -492,7 +521,7 @@ class QAModel(object):
             epoch_tic = time.time()
 
             # Loop over batches
-            for batch in get_batch_generator(self.word2id, train_context_path, train_qn_path, train_ans_path, self.FLAGS.batch_size, context_len=self.FLAGS.context_len, question_len=self.FLAGS.question_len, discard_long=True):
+            for batch in get_batch_generator(self.word2id, train_context_path, train_qn_path, train_ans_path, self.FLAGS.batch_size, context_len=self.FLAGS.context_len, question_len=self.FLAGS.question_len, word_len=self.FLAGS.word_len, discard_long=True):
 
                 # Run training iteration
                 iter_tic = time.time()